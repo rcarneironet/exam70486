6756856348812ccac1f21db0ea1603020b27fba7
Ray Carneiro <rcarneironet@gmail.com>
2018-10-29 21:53:26 -0300

    Initial commit

---
+++ AzureQueueStorage/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Threading;
+
+namespace AzureQueueStorage
+{
+    class Program
+    {
+        private const string StorageAccountName = "rcarneiroazurestorage";
+        private const string StorageAccountKey = "rwLKv18/7lG7lSkjhmEnujJUtX9omtkKo3ZscUlYNYuY7KoPUW6KYSqj9N2rax7YciO7U6ZLruWXxzuh4cNyvg==";
+
+        static void Main(string[] args)
+        {
+          
+            //enviando
+            var storageAccount = new CloudStorageAccount(
+                new StorageCredentials(StorageAccountName, StorageAccountKey),
+                true);
+
+            var client = storageAccount.CreateCloudQueueClient();
+
+            var queue = client.GetQueueReference("minha-fila");
+
+            queue.CreateIfNotExistsAsync();
+
+            for (int i = 0; i < 5; i++)
+            {
+                queue.AddMessageAsync(new CloudQueueMessage($"Mensagem numero: {i}"));
+            }
+
+            Console.WriteLine("Completed!");
+            Console.ReadKey();
+
+            //consumidor
+
+            while (true)
+            {
+                var message = queue.GetMessageAsync();
+
+                if (message != null)
+                {                                      
+                    Console.WriteLine("Retrieved message");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
